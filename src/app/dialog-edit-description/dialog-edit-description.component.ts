c3df3829c59d4dc4c68d09177ff68f222b250c8b
Oleg Benets <oleg.benets@web.de>
2024-09-25 16:24:59 +0200

    implemented edit function on each detail

--- src/app/dialog-edit-description/dialog-edit-description.component.ts
+++ src/app/dialog-edit-description/dialog-edit-description.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
@@ -24,17 +24,25 @@ import { MatIcon } from '@angular/material/icon';
   templateUrl: './dialog-edit-description.component.html',
   styleUrl: './dialog-edit-description.component.scss'
 })
-export class DialogEditDescriptionComponent {
+export class DialogEditDescriptionComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<DialogEditDescriptionComponent>, public productService: ProductDataService) {}
 
   product = new Product();
   productId = '';
   currentDetail: string = '';
   loading: boolean = false;
+  editingDetail: string | null = null;  
+  editingValue: string = ''; 
+  temporaryDetails: string[] = [];
+
+  ngOnInit(): void {
+    this.temporaryDetails = [...this.product.detail]; 
+  }
 
  async saveProduct(productForm: NgForm) {
   if (productForm.form.valid) {
     this.loading = true;
+    this.product.detail = [...this.temporaryDetails];
 
   await this.productService.updateProduct(this.productId, this.product);
       this.loading = false;
@@ -43,20 +51,36 @@ export class DialogEditDescriptionComponent {
   }
 
   addDetail() {
-    if (this.currentDetail.trim() !== '' && !this.product.detail.includes(this.currentDetail)) {
-      this.product.detail.push(this.currentDetail);
+    if (this.currentDetail.trim() !== '' && !this.temporaryDetails.includes(this.currentDetail)) {
+      this.temporaryDetails.push(this.currentDetail);
       this.currentDetail = '';
     }
   }
 
-  editDetail(detail: string) {
+  setEditing(detail: string) {
+    this.editingDetail = detail;           
+    this.editingValue = detail;       
+  }
 
+  finishEditing(originalDetail: string) {
+    if (this.editingValue.trim() !== '') {
+      let index = this.temporaryDetails.indexOf(originalDetail);
+      if (index !== -1) {
+        this.temporaryDetails[index] = this.editingValue;  
+      }
+    }
+    this.cancelEditing();  
+  }
+
+  cancelEditing() {
+    this.editingDetail = null;              
+    this.editingValue = '';           
   }
 
   deleteDetail(detail: string) {
-    let index = this.product.detail.indexOf(detail);
+    let index = this.temporaryDetails.indexOf(detail);
     if (index !== -1) {
-      this.product.detail.splice(index, 1);
+      this.temporaryDetails.splice(index, 1);
     }
   }
 }
