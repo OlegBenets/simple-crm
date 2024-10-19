d30c2c759447ce1902c1fb21064636169300dd80
Oleg Benets <oleg.benets@web.de>
2024-10-19 15:59:04 +0200

    fixed sing up and login e2e test

--- cypress/e2e/auth.spec.cy.ts
+++ cypress/e2e/auth.spec.cy.ts
@@ -6,21 +6,23 @@ describe('The Home Page', () => {
 
 describe('Authentication Tests', () => {
   it('should signup and login', () => {
+
     cy.visit('/login');
-    cy.contains('Sign up').click(); 
+    cy.contains('Sign up').click();
     cy.url().should('include', '/signup');
 
-    cy.get('input[formControlName="firstName"]').should('be.visible').click().type('John'); 
-    cy.get('input[formControlName="lastName"]').should('be.visible').click().type('Doe'); 
-    cy.get('input[formControlName="email"]').should('be.visible').click().type('johndoe@example.com'); 
-    cy.get('input[formControlName="password"]').should('be.visible').click().type('Password123!'); 
+    cy.get('input[formControlName="firstName"]').click({ force: true }).type('John'); 
+    cy.get('input[formControlName="lastName"]').click({ force: true }).type('Doe'); 
+    cy.get('input[formControlName="email"]').click({ force: true }).type('johndoe@example.com'); 
+    cy.get('input[formControlName="password"]').click({ force: true }).type('Password123!'); 
 
-    cy.contains('Sign Up').click();
+    cy.get('button[type="submit"]').click(); 
     cy.url().should('include', '/login');
-    
+
     cy.get('input[formControlName="email"]').should('be.visible').click().type('johndoe@example.com'); 
     cy.get('input[formControlName="password"]').should('be.visible').click().type('Password123!');
-    cy.contains('Log in').click(); 
+   
+    cy.get('button[type="submit"]').click(); 
     cy.url().should('include', '/dashboard');
 
     cy.contains('All Users').should('be.visible');
